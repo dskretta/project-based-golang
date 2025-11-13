214dc4bc10511f624e809014d119ecc4d732928d
Dimitrios Skrettas <121012738+dskretta@users.noreply.github.com>
2025-11-13 16:47:15 -0500

    Add log searcher functionality with user input
    Implement log searcher that reads directory and prompts for user input.

--- week2/LogSearcherV2/logSearcherV2.go
+++ week2/LogSearcherV2/logSearcherV2.go
@@ -1,1 +1,39 @@
+package main
 
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Declare variables and get User Input
+	fmt.Println("Enter the directory path!")
+	var dirPath string
+	fmt.Scanln(&dirPath)
+
+	fmt.Println("Enter the keyword we are searching for!")
+	var keyword string
+	fmt.Scanln(&keyword)
+
+	// Read the directory contents
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	for_, entry := range entries {
+		fileName := entry.Name() // get's the file name
+
+		if !entry.IsDir() {
+			//It's a file!
+			//Search logic
+		} else {
+			//It's a directory so skip?
+			fmt.Println(dirPath+"/"entry.Name, " is a directory, skipping for now")
+		}
+	}
+
+}
